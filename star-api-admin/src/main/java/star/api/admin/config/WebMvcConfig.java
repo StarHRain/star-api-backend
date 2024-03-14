41cd48926f44183037ee95f4dad0bc99d949b08d
Starrain <2324532342@qq.com>
2024-03-15 01:57:42 +0800

    3.14 利用Redis缓存解决了分布式Session共享问题 控制器拦截所有方法刷新缓存时间

---
+++ star-api-admin/src/main/java/star/api/admin/config/WebMvcConfig.java
@@ -0,0 +1,26 @@
+package star.api.admin.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import star.api.admin.interceptor.TokenRefreshInterceptor;
+
+import javax.annotation.Resource;
+
+/**
+ * @author 千树星雨
+ * @date 2024 年 03 月 15 日
+ */
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    private RedisTemplate<String,Object> redisTemplate;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new TokenRefreshInterceptor(redisTemplate))
+                .addPathPatterns("/**");
+    }
+}
---
+++ star-api-admin/src/main/java/star/api/admin/constant/RedisConstant.java
@@ -0,0 +1,10 @@
+package star.api.admin.constant;
+
+/**
+ * @author 千树星雨
+ * @date 2024 年 03 月 14 日
+ */
+public class RedisConstant {
+    public static final String LOGIN_TOKEN_KEY = "star:api:admin:login:token:";
+    public static final Long LOGIN_TOKEN_TTL = 5L;
+}
